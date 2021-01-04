7216b3b7d79a7ee1ec958e0da36efa7560c9a527
Adam Figueroa <admiralfiggins@gmail.com>
2021-01-04 09:45:59 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const playStore = require("./playstore");
+
+const app = express();
+const morganMiddlewareFunc = morgan("common");
+
+app.use(morganMiddlewareFunc);
+
+app.get("/apps", (req, res) => {
+  let playList = [...playStore];
+  let { genres, sort } = req.query;
+  const possibleGenres = [
+    "action",
+    "puzzle",
+    "strategy",
+    "casual",
+    "arcade",
+    "card",
+  ];
+  let genresFilter = [];
+
+  const capitalize = (s) => {
+    if (typeof s !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
+  }
+
+  if (genres !== undefined && !possibleGenres.includes(genres.toLowerCase())) {
+    return res
+      .status(400)
+      .send("Genre must be: action, puzzle, strategy, casual, arcade, or card");
+  }
+
+  if (genres) {
+    genresFilter = playList.filter((playApp) =>
+      playApp.Genres.toLowerCase().includes(genres.toLowerCase())
+    );
+  }
+
+  if (genres === undefined) {
+      genresFilter = playList
+  }
+
+  if (sort !== undefined && sort.toLowerCase() !== "rating" && sort.toLowerCase() !== "app") {
+    return res
+    .status(400)
+    .send('Sort must be either "Rating" or "Rpp"');
+  }
+
+  if (sort) {
+      sort = capitalize(sort);
+      if (sort === 'Rating') {
+        genresFilter.sort((currApp, nextApp) => {
+            if (currApp[sort] < nextApp[sort]) {
+                return 1;
+            } else if (nextApp[sort] < currApp[sort]) {
+                return -1;
+            } else return 0;
+          });
+      } else {
+        genresFilter.sort((currApp, nextApp) => {
+            if (currApp[sort] < nextApp[sort]) {
+                return -1;
+            } else if (nextApp[sort] < currApp[sort]) {
+                return 1;
+            } else return 0;
+          });
+      }
+  };
+
+  res.json(genresFilter);
+});
+
+app.listen(8000, () => {
+  console.log("Express server is listening on port 8000!");
+});
